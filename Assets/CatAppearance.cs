c5c030e61deaa4b56e1bfe6aecc2fe8f811175a9
Alice <jade0@hotmail.fr>
2020-11-22 18:22:46 +0100

    Cat
    Add a cat randomly somewhere in the house

---
+++ Assets/CatAppearance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAppearance : MonoBehaviour
+{
+    public List<Transform> myAvailablePosition;
+    public GameObject myCat;
+
+    void Awake()
+    {
+        if (myAvailablePosition.Count > 0)
+        {
+            myCat.SetActive(true);
+            int randomPosition = Random.Range(0, myAvailablePosition.Count - 1);
+            Debug.Log(randomPosition);
+            myCat.transform.position = myAvailablePosition[randomPosition].transform.position;
+        }
+        else
+        {
+            myCat.SetActive(false);
+        }
+    }
+
+}
