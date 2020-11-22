c259f350f613210f9f8110497f9c82bee476b131
Alice <jade0@hotmail.fr>
2020-11-22 15:16:29 +0100

    Home collider + Teleport
    Add collider on player and on tilemap, so we can navigate between floors

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject myTeleportEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("enter collision");
+        col.gameObject.transform.position = myTeleportEnd.transform.position;
+    }
+}
