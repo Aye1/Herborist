786950906eda274a8dd837bda2c64751593a8c43
Alice <jade0@hotmail.fr>
2020-11-22 17:05:19 +0100

    Teleport prefab

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public GameObject myTeleportEnd;
+    public bool myNeedInteraction = false;
+    private GameObject myPlayer = null;
     // Start is called before the first frame update
     void Start()
     {
     }
 
+    private void Update()
+    {
+        if(myNeedInteraction && myPlayer != null && Input.GetKeyDown(KeyCode.A))
+        {
+            TeleportPlayer();
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("enter collision");
-        col.gameObject.transform.position = myTeleportEnd.transform.position;
+        myPlayer = col.gameObject;
+        if (myNeedInteraction)
+        {
+
+        }
+        else
+        {
+            TeleportPlayer();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        myPlayer = null;
+    }
+
+    private void TeleportPlayer()
+    {
+        myPlayer.transform.position = myTeleportEnd.transform.position;
     }
 }
