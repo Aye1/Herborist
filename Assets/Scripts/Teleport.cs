cf833c7052f1a81d7757b01ac73f8aa19212f015
Alice <jade0@hotmail.fr>
2020-12-30 16:50:53 +0100

    Restore teleport by door

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,42 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Teleport : MonoBehaviour , IInteractable
+public class Teleport : MonoBehaviour 
 {
     public GameObject myTeleportEnd;
-    public bool myNeedInteraction = false;
 
-    void OnTriggerEnter2D(Collider2D col)
+    protected virtual void OnTriggerEnter2D(Collider2D col)
     {
-        if (myNeedInteraction == false)
-        { 
-            TeleportPlayer(col.gameObject);
-        }
+         TeleportPlayer(col.gameObject);
     }
 
-    private void TeleportPlayer(GameObject aPlayer)
+    protected void TeleportPlayer(GameObject aPlayer)
     {
         aPlayer.transform.position = myTeleportEnd.transform.position;
-    }
-
-    public void Interact(GameObject aPlayer)
-    {
-        TeleportPlayer(aPlayer);
-    }
-
-    public bool CanInteract()
-    {
-        return true;
-    }
-
-    public GameObject GetGameObject()
-    {
-        return gameObject;
-    }
-
-    public string GetInteractionTextLocKey()
-    {
-        return "teleport_interaction";
     }
 }
