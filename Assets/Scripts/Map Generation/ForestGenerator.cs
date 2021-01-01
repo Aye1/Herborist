46b6cb4b0dd94fcc20afcea9f1543369bb69d07a
Adrien Janvier <janvier.adrien@gmail.com>
2021-01-01 19:27:47 +0100

    Greatly improve forest generation

--- Assets/Scripts/Map Generation/ForestGenerator.cs
+++ Assets/Scripts/Map Generation/ForestGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.Diagnostics;
 
 public class ForestGenerator : MonoBehaviour
 {
@@ -9,17 +10,17 @@ public class ForestGenerator : MonoBehaviour
     [Range(0.0f, 1.0f)] public float density = 1.0f;
     [Range(0.0f, 1.0f)] public float noise = 0.5f;
 
-    // TODO: someday check how this is supposed to work and how it affects position offset
-    [HideInInspector]public float stepSize = 1.0f;
+    public float stepSize = 1.0f;
     public int numberBalances = 0;
     public List<GameObject> treesTemplates;
     [HideInInspector]
     public List<Vector2Int> posToAvoid;
 
     private Dictionary<Vector2Int, GameObject> _createdTrees;
-    private List<Vector2Int> _generatedPositions;
     private int _numberSteps;
     private Vector3 _offset;
+
+    private bool[,] _treePositions;
 
     public IEnumerable<GameObject> Trees
     {
@@ -40,45 +41,46 @@ public class ForestGenerator : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        _offset = new Vector3(0.0f, size, 0.0f);
+        _offset = new Vector3(-size * 0.5f, 0.0f, 0.0f);
         _numberSteps = (int)(size / stepSize);
         posToAvoid = new List<Vector2Int>();
     }
 
     public void LaunchTreesGeneration()
     {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
         GenerateRandomTrees();
         for(int i = 0; i < numberBalances; i++)
         {
             BalanceTrees();
         }
-        CreateTrees(_generatedPositions);
+        CreateTrees();
+        sw.Stop();
+        UnityEngine.Debug.Log("Time elapsed: " + sw.Elapsed);
     }
 
     public bool HasTreeAtPos(Vector2Int position)
     {
-        return _generatedPositions.Contains(position);
+        Vector2Int internalPosition = TilemapToInternalPosition(position);
+        return IsInGrid(internalPosition) && _treePositions[internalPosition.x, internalPosition.y];
     }
 
     private void GenerateRandomTrees()
     {
         _createdTrees = new Dictionary<Vector2Int, GameObject>();
-        _generatedPositions = new List<Vector2Int>();
+        _treePositions = new bool[_numberSteps, _numberSteps];
         if(density == 0.0f)
         {
             return;
         }
-        for (int i = 0; i < 2 * _numberSteps; i++)
+        for (int i = 0; i < _numberSteps; i++)
         {
-            for (int j = 0; j < 2 * _numberSteps; j++)
+            for (int j = 0; j < _numberSteps; j++)
             {
-                if(Alea.GetFloat(0.0f, 1.0f) <= density && !MustBeAvoided(i, j))
-                {
-                     _generatedPositions.Add(new Vector2Int(i, j));
-                }
+                _treePositions[i, j] = Alea.GetFloat(0.0f, 1.0f) <= density && !MustBeAvoided(InternalToTilemapPosition(new Vector2Int(i,j)));
             }
         }
     }
@@ -87,41 +89,72 @@ public class ForestGenerator : MonoBehaviour
     {
         List<Vector2Int> treesToDestroy = new List<Vector2Int>();
         List<Vector2Int> treesToCreate = new List<Vector2Int>();
-        for (int i = 0; i < 2 * _numberSteps; i++)
+        for (int i = 0; i < _numberSteps; i++)
         {
-            for (int j = 0; j < 2 * _numberSteps; j++)
+            for (int j = 0; j < _numberSteps; j++)
             {
                 bool isSurrounded = IsPositionSurroundedByTrees(new Vector2Int(i, j));
                 Vector2Int position = new Vector2Int(i, j);
+                Vector2Int realPosition = InternalToTilemapPosition(position);
                 if(HasTreeAtPosition(position) && !isSurrounded)
                 {
                     treesToDestroy.Add(position);
-                } else if (!HasTreeAtPosition(position) && isSurrounded && !MustBeAvoided(position))
+                } else if (!HasTreeAtPosition(position) && isSurrounded && !MustBeAvoided(realPosition))
                 {
                     treesToCreate.Add(position);
                 }
             }
         }
-        _generatedPositions.RemoveAll(t => treesToDestroy.Contains(t));
-        _generatedPositions.AddRange(treesToCreate);
+
+        // Change the values after checking the whole map, as any change will have impact on the algorithm
+        foreach(Vector2Int pos in treesToDestroy)
+        {
+            _treePositions[pos.x, pos.y] = false;
+        }
+        foreach(Vector2Int pos in treesToCreate)
+        {
+            _treePositions[pos.x, pos.y] = true;
+        }
     }
 
     private bool IsPositionSurroundedByTrees(Vector2Int position)
     {
-        return GetTreeNeighbours(position).Count() >= 4;
+        return GetNeighboursCount(position) >= 4;
     }
 
-    private IEnumerable<Vector2Int> GetTreeNeighbours(Vector2Int position)
+    private int GetNeighboursCount(Vector2Int position)
     {
-        return _generatedPositions.Where(p =>
+        int count = 0;
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
             {
-                return Mathf.Abs(position.x - p.x) <= 1 && Mathf.Abs(position.y - p.y) <= 1;
-            });
+                int x = position.x + i;
+                int y = position.y + j;
+
+                // Thanks && is lazy and does not compute right part if left part is false
+                if (IsInGrid(x, y) && _treePositions[x, y])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private bool IsInGrid(int x, int y)
+    {
+        return x >= 0 && x < _numberSteps && y >= 0 && y < _numberSteps ;
+    }
+
+    private bool IsInGrid(Vector2Int position)
+    {
+        return IsInGrid(position.x, position.y);
     }
 
     private bool HasTreeAtPosition(Vector2Int position)
     {
-        return _generatedPositions.Contains(position);
+        return _treePositions[position.x, position.y];
     }
 
     private bool MustBeAvoided(Vector2Int position)
@@ -144,14 +177,20 @@ public class ForestGenerator : MonoBehaviour
 
     private void DestroyTree(Vector2Int position)
     {
-        _generatedPositions.Remove(position);
+        _treePositions[position.x, position.y] = false;
     }
 
-    private void CreateTrees(IEnumerable<Vector2Int> treeList)
+    private void CreateTrees()
     {
-        foreach(Vector2Int pos in treeList)
+        for(int i = 0; i < _numberSteps; i++)
         {
-            CreateTree(pos);
+            for(int j = 0; j < _numberSteps; j++)
+            {
+                if(_treePositions[i, j])
+                {
+                    CreateTree(new Vector2Int(i, j));
+                }
+            }
         }
     }
 
@@ -159,7 +198,8 @@ public class ForestGenerator : MonoBehaviour
     {
         float noiseX = Alea.GetFloat(0.0f, noise);
         float noiseY = Alea.GetFloat(0.0f, noise);
-        Vector3 pos = new Vector3((position.x + noiseX - _numberSteps) * stepSize, (position.y + noiseY - _numberSteps) * stepSize, 0.0f) + _offset;
+        Vector2 realPos = InternalToRealContinuousPosition(position);
+        Vector3 pos = new Vector3(realPos.x + noiseX, realPos.y + noiseY, 0.0f) + _offset;
         GameObject tree = GenerateTree(pos);
         _createdTrees.Add(new Vector2Int(position.x, position.y), tree);
     }
@@ -171,5 +211,20 @@ public class ForestGenerator : MonoBehaviour
         tree.transform.localPosition = position;
         tree.transform.localScale = Vector3.one;
         return tree;
+    }
+
+    private Vector2 InternalToRealContinuousPosition(Vector2Int position)
+    {
+        return new Vector2(position.x * stepSize, position.y * stepSize);
+    }
+
+    private Vector2Int InternalToTilemapPosition(Vector2Int position)
+    {
+        return new Vector2Int(Mathf.FloorToInt(position.x * stepSize), Mathf.FloorToInt(position.y * stepSize));
+    }
+
+    private Vector2Int TilemapToInternalPosition(Vector2Int position)
+    {
+        return new Vector2Int(Mathf.FloorToInt(position.x / stepSize), Mathf.FloorToInt(position.y / stepSize));
     }
 }
