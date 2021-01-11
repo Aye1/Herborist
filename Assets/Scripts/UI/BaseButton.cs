20cd12c886af3c11dda9ec29ae51d3fdfdd783d7
Adrien Janvier <janvier.adrien@gmail.com>
2021-01-11 17:54:18 +0100

    Currently selected button is bigger than others

---
+++ Assets/Scripts/UI/BaseButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BaseButton : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    private Vector3 _scale;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        _scale = transform.localScale;
+        transform.localScale = 1.1f * Vector3.one; 
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        transform.localScale = _scale;
+    }
+
+}
