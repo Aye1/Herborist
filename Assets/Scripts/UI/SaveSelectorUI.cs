f501dde7d5b82eebb83b2e0703f1bda2131a9109
Adrien Janvier <janvier.adrien@gmail.com>
2021-01-16 13:56:51 +0100

    Step further on save selection UI

--- Assets/Scripts/UI/SaveSelectorUI.cs
+++ Assets/Scripts/UI/SaveSelectorUI.cs
@@ -1,7 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
 
 public class SaveSelectorUI : MonoBehaviour
 {
+    [SerializeField, Required, ChildGameObjectsOnly]
+    public List<GameObject> saveButtons;
+
+    private void OnEnable()
+    {
+        SetKeyboardFocus();
+    }
+
+    private void SetKeyboardFocus()
+    {
+        if (saveButtons != null && saveButtons.Count > 0)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(saveButtons[0]);
+        }
+    }
 }
