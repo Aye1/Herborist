730002e35057170ac7efa9657135318d6113413e
Adrien Janvier <janvier.adrien@gmail.com>
2021-02-13 14:49:48 +0100

    Add parent plant to plant components

--- Assets/Scripts/ScriptableObjects/PlantScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/PlantScriptableObject.cs
@@ -6,23 +6,11 @@ using Sirenix.OdinInspector;
 [CreateAssetMenu(fileName = "Plant", menuName = "ScriptableObjects/Plant")]
 public class PlantScriptableObject : ScriptableObject
 {
-    public string unidentifiedNameLocKey;
+    public string unidentifiedPlantLetter;
     public string commonNameLocKey;
     public string speciesName;
     public string familyName;
     public List<string> loreLocKey;
     public Sprite fullPicture;
     public List<PlantComponentScriptableObject> components;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
