aefe12a3e0e04101d78fb195efa1b435e40ac59b
Adrien Janvier <janvier.adrien@gmail.com>
2021-02-13 15:18:35 +0100

    Identification questions

---
+++ Assets/Scripts/ScriptableObjects/IdentificationQuestionsScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Identification Questions", menuName ="ScriptableObjects/Identification Questions")]
+public class IdentificationQuestionsScriptableObject : ScriptableObject
+{
+    public List<PlantIdentificationParameterScriptableObject> questions;
+}
