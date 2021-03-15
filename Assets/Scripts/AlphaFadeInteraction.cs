8ca3a05b33e1cea16e4cc7d7476e224054189d56
Adrien Janvier <janvier.adrien@gmail.com>
2021-03-15 16:36:24 +0100

    Alpha fading when player near/behind trees

---
+++ Assets/Scripts/AlphaFadeInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaFadeInteraction : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject != gameObject)
+            ChangeAlpha(0.5f, collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject != gameObject)
+            ChangeAlpha(1.0f, collision.gameObject);
+    }
+
+    private void ChangeAlpha(float targetAlpha, GameObject obj)
+    {
+        foreach(SpriteRenderer rend in obj.GetComponents<SpriteRenderer>())
+        {
+            Color tmp = rend.color;
+            rend.color = new Color(tmp.r, tmp.g, tmp.b, targetAlpha);
+        }
+
+        foreach(SpriteRenderer rend in obj.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Color tmp = rend.color;
+            rend.color = new Color(tmp.r, tmp.g, tmp.b, targetAlpha);
+        }
+    }
+}
